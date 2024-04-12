c6ba2d3eee9ced2077d4c7306c0ecf2bc2b3eb3b
gmk <gmk@example.com>
2024-04-12 13:38:27 +0530

    changes in suggestions

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -6,10 +7,11 @@ import pickle
 from tqdm.notebook import tqdm
 from PIL import Image
 import os
+import io
 
-# Function to generate caption for the image
 
-st.set_page_config(page_title='Visual Captioning')
+
+st.set_page_config(page_title='Visual Captioning',layout="wide")
 
 def idx_to_word(integer, tokenizer):
     for word, index in tokenizer.word_index.items():
@@ -49,6 +51,7 @@ for line in tqdm(captions_doc.split('\n')):
 model = load_model(os.path.join(current_directory, 'captioning_model.h5'))
 max_length = 35
 
+#Function to predict caption
 def predict_caption(model, image, tokenizer, max_length):
     in_text = 'st'
     for _ in range(max_length):
@@ -64,28 +67,46 @@ def predict_caption(model, image, tokenizer, max_length):
             break
     return in_text
 
+#Function to generate caption
 def generate_caption(image_file):
     image_id = os.path.splitext(image_file)[0]
     captions = mapping.get(image_id, [])
+    st.markdown(
+    """
+    <style>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+         /* Adjust the height as needed */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
     
     if not captions:
-        st.write("oopsss!!!! Cannot predict for this")
+        st.markdown(f"<p class='center'>oopsss!!!! Cannot predict for this</p>", unsafe_allow_html=True)
+        return ""
+    else:        
 
-    else:
-        st.write('---------------------Actual---------------------')
+        st.markdown("<h6 style='text-align: center;'>---------------------Actual--------------------- </h6>", unsafe_allow_html=True)
+        
         for caption in captions:
-            st.write(caption)
+            st.markdown(f"<p class='center'>{caption}</p>", unsafe_allow_html=True)
         y_pred = predict_caption(model, features[image_id], tokenizer, max_length)
-        st.write('--------------------Predicted--------------------')
+        st.markdown("<h6 style='text-align: center;'>---------------------Predicted--------------------- </h6>", unsafe_allow_html=True)
         return y_pred
+    
     
 
     
 def main():
-    st.title("Visual Caption Generator")
-    st.caption("Aims to generate captions for the images:blush:")
+    st.markdown('<h1 style="text-align: center;">Visual Caption Generator</h1>', unsafe_allow_html=True)
+    st.markdown('<p style="text-align: center; color: #787878;">Aims to generate captions for the images 😊</p>', unsafe_allow_html=True)    
     st.write("These are some suggested images,, click on them to see the predicted captions.... ")
     st.write("Scroll down for the custom images:wink:")
+    #Displaying suggested images
     images = []
     image_names = []
     folder_path=os.path.join(current_directory, 'Display')
@@ -99,25 +120,42 @@ def main():
     b=None
     st.write("<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html=True)
     for i in range(len(images)):
-        if (i % 5) == 0:
-            row = st.columns(5)
-        with row[i % 5]:
+        if (i % 10) == 0:
+            row = st.columns(10)
+        with row[i % 10]:
             st.image(images[i], use_column_width=True)
             if st.button("Generate",key={image_names[i]}):
                 a=images[i]
                 b=image_names[i]
                 placeholder = st.empty()
                 placeholder.info("Please scroll down !!.")
+    
                 
     if a != None and b != None:
-        st.image(a, caption="", use_column_width=True)
+        desired_width = 500  # Change this to your preferred width
+        image_resized = a.resize((desired_width, int(desired_width * a.height / a.width)))
+        buffered = io.BytesIO()
+        image_resized.save(buffered, format="JPEG")
+        image_str = base64.b64encode(buffered.getvalue()).decode()
+
+        st.markdown(
+        f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/jpeg;base64,{image_str}" alt="Uploaded Image" style="max-width: {desired_width}px;">
+        </div>
+        """,
+        unsafe_allow_html=True
+        )
+
         k=generate_caption(b)
-        st.write(k)
-        
+        st.markdown(f"<p class='center'>{k}</p>", unsafe_allow_html=True)
+    
     st.write("<br>", unsafe_allow_html=True)
     st.write("<br>", unsafe_allow_html=True)
     st.write("<br>", unsafe_allow_html=True)
     st.write("<br>", unsafe_allow_html=True)
+
+    #Custom images
     st.markdown("## Choose an Image")
     uploaded_file = st.file_uploader("", type=["jpg"])
     st.write("Note: Custom images should be from FLICKR 8K dataset")
@@ -125,10 +163,26 @@ def main():
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="", use_column_width=True)
+        desired_width = 500  # Change this to your preferred width
+        image_resized = image.resize((desired_width, int(desired_width * image.height / image.width)))
+        buffered = io.BytesIO()
+        image_resized.save(buffered, format="JPEG")
+        image_str = base64.b64encode(buffered.getvalue()).decode()
+
+        st.markdown(
+        f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/jpeg;base64,{image_str}" alt="Uploaded Image" style="max-width: {desired_width}px;">
+        </div>
+        """,
+        unsafe_allow_html=True
+        )
+
+        
+    
         if st.button("Generate Caption"):
             caption = generate_caption(uploaded_file.name)
-            st.write(caption)
+            st.markdown(f"<p class='center'>{caption}</p>", unsafe_allow_html=True)
 
     
 
